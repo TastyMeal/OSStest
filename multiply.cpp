897e728ba22c0b766b2c0a5733e4c76f914700df
JihoChoi <arcueidd@gmail.com>
2018-04-13 10:15:07 +0900

    1차 수정

--- multiply.cpp
+++ multiply.cpp
@@ -6,9 +6,9 @@ void main()
 	int i=0,j=0;
 
 	// 1~8단 까지만 출력
-	for(i=1;i<10;i++)
+	for(i=1;i<9;i++)
 	{
-		for(j=1;j<9;j++)
+		for(j=1;j<10;j++)
 		{
 			cout << a << " multiply " << b << " = " << a*b;
 			cout << endl;
